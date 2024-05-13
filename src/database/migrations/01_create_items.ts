6e39ed3d51887bb70b349ff5098ee8c88f339e73
Giacomo Salsano <giacomosalsano@hotmail.com>
2024-05-13 14:56:11 -0300

    created tables

--- src/database/migrations/01_create_items.ts
+++ src/database/migrations/01_create_items.ts
@@ -1,5 +1,15 @@
 import Knex from 'knex';
 
-export async function up() {}
+export async function up(Knex: Knex) {
+   return Knex.schema.createTable('items', table => {
+        table.increments('id').primary();
+        table.string('image').notNullable();
+        table.string('title').notNullable();
+       
+    });
+}
 
-export async function down() {}
+export async function down(Knex: Knex) {
+   return Knex.schema.dropTable('items');
+};
+
--- src/database/migrations/02_create_point_items.ts
+++ src/database/migrations/02_create_point_items.ts
@@ -1,3 +1,20 @@
-export async function up() {}
+import Knex from 'knex';
 
-export async function down() {}
+export async function up(Knex: Knex) {
+   return Knex.schema.createTable('point_items', table => {
+        table.increments('id').primary();
+
+        table.integer('point_id').notNullable()
+        .references('id')
+        .inTable('points');
+
+        table.integer('item_id').notNullable()
+        .references('id')
+        .inTable('items');
+    });
+}
+
+export async function down(Knex: Knex) {
+   return Knex.schema.dropTable('point_items');
+};
+
