ea76df042a0b002a181204df444311f405d6dcc4
Giacomo Salsano <giacomosalsano@hotmail.com>
2024-05-14 17:57:23 -0300

    created multer.ts

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -57,7 +57,7 @@ class PointsController{
         const trx = await knex.transaction();
 
         const point = {
-            image: request.file.filename,
+            image: 'https://images.unsplash.com/photo-1601600576337-c1d8a0d1373c?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3VwZXJtYXJrZXR8ZW58MHx8MHx8fDA%3D',
             name,
             email,
             whatsapp,
