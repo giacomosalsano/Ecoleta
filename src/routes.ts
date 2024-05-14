32753a84f67596bf67d285b61ab530e47c55205a
Giacomo Salsano <giacomosalsano@hotmail.com>
2024-05-14 14:39:30 -0300

    created Items&PointsController's

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,26 @@
 import express from 'express';
+import knex from './database/connection';
+import PointsController from './controllers/pointsController';
+import { number } from '@hapi/joi';
 
 const routes = express.Router();
+const pointsController = new PointsController();
 
-routes.get('/',(request, response) => {
-    return response.json({ message: 'Hello World! How are you?'});
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return {
+            id: item.id,
+            title: item.title,
+            image_url: `http://192.168.0.58:3333/uploads/${item.image}`,
+        };
+    });
+
+    return response.json({serializedItems, message: 'Hello World! How are you?'});
+
 });
+
+routes.post('/points', pointsController.create);
 
 export default routes;
