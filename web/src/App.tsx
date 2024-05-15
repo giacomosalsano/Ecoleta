67a008931c28eebd4298e8a4854727eefcd79c6c
Giacomo Salsano <giacomosalsano@hotmail.com>
2024-05-15 13:01:20 -0300

    created Home & index.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 
+import Home from './pages/Home';
+
 function App() {
   return (
-    <h1>Hello World! How are you?</h1>
+    <Home />
   );
 }
-
 export default App;
